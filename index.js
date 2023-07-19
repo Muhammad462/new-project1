3e17c9baf69564fd9c11c729d549ea997455b7d1
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-19 16:34:26 +0500

    a

---
+++ index.js
@@ -0,0 +1,3 @@
+// My new project
+var name3 = "Ali";
+console.log("Hello my name is ".concat(name3, " and this is my first program"));
---
+++ index.ts
@@ -0,0 +1,3 @@
+// My new project
+let name3 = "Ali";
+console.log(`Hello my name is ${name3} and this is my first program`)
